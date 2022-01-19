b5eb04dd28ae77b1c5c3144200062d50c4e743f0
Joel <joel.ari.howard@gmail.com>
2022-01-19 13:18:13 -0700

    pandas video

--- runTest.py
+++ runTest.py
@@ -1,9 +1,20 @@
-from qutip import basis, tensor
 import pandas as pd
+import numpy as np
 
 names = ['Bob', 'Jessica', 'Mary', 'John', 'Mel']
-births = [968, 155, 77, 578, 973]
-BabyDataSet = list(zip(names, births))
-df = pd.DataFrame(data=BabyDataSet, columns=['Names', 'Births'])
+ages = list(range(10,15,1))
+genders = ['M','F','F','M','F']
+BabyDataSet = list(zip(names, ages, genders))
+print(BabyDataSet)
+df = pd.DataFrame(data=BabyDataSet, columns=['Names', 'Births', 'Genders'])
+
 # print(df)
-print(df.sort_values(['Births'], ascending=False))
+# print(df['Names'])
+# print(df.Names)
+# print(df[['Names', 'Genders']])
+# print(df.iloc[0]) #Integer location
+# for index, row in df.iterrows():
+#     print(index, row['Name'])
+print(df.loc[df['Genders'] == "M"])
+# print(df.sort_values(['Births'], ascending=False))
+# print(df['Births'])
