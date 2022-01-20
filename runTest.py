79405d10ee4517e88a3715b8e054927d95272625
Joel <joel.ari.howard@gmail.com>
2022-01-19 21:43:23 -0700

    commented everything

--- runTest.py
+++ runTest.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 
-names = ['Bob', 'Jessica', 'Mary', 'John', 'Mel']
-ages = list(range(10,15,1))
-genders = ['M','F','F','M','F']
-BabyDataSet = list(zip(names, ages, genders))
-print(BabyDataSet)
-df = pd.DataFrame(data=BabyDataSet, columns=['Names', 'Births', 'Genders'])
+# names = ['Bob', 'Jessica', 'Mary', 'John', 'Mel']
+# ages = list(range(10,15,1))
+# genders = ['M','F','F','M','F']
+# BabyDataSet = list(zip(names, ages, genders))
+# print(BabyDataSet)
+# df = pd.DataFrame(data=BabyDataSet, columns=['Names', 'Births', 'Genders'])
 
 # print(df)
 # print(df['Names'])
@@ -15,6 +15,6 @@ df = pd.DataFrame(data=BabyDataSet, columns=['Names', 'Births', 'Genders'])
 # print(df.iloc[0]) #Integer location
 # for index, row in df.iterrows():
 #     print(index, row['Name'])
-print(df.loc[df['Genders'] == "M"])
+# print(df.loc[df['Genders'] == "M"])
 # print(df.sort_values(['Births'], ascending=False))
 # print(df['Births'])
